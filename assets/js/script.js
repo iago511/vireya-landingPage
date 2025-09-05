07328b4dab4de5b1b19b8cb97132d186552b87b9
EnzoHerrera <enzo.dinks@gmail.com>
2025-09-05 10:48:30 -0300

    Adicionando o js e responsividade a página de cadastro

---
+++ assets/js/script.js
@@ -0,0 +1,13 @@
+document.getElementById('togglePassword').addEventListener('click', function () {
+    const passwordInput = document.getElementById('senha');
+    const eyeIcon = this.querySelector('i');
+    if (passwordInput.type === 'password') {
+        passwordInput.type = 'text';
+        eyeIcon.classList.remove('fa-eye');
+        eyeIcon.classList.add('fa-eye-slash');
+    } else {
+        passwordInput.type = 'password';
+        eyeIcon.classList.remove('fa-eye-slash');
+        eyeIcon.classList.add('fa-eye');
+    }
+});
